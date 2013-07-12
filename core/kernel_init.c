10f587db5ed67c936d65bb65e79f22dd15881a6f
southernbear <xq5530303@gmail.com>
2013-07-12 09:55:49 +0800

    Sleep when IDLE

--- core/kernel_init.c
+++ core/kernel_init.c
@@ -43,14 +43,7 @@ extern int main(void);
 static void idle_thread(void)
 {
     while (1) {
-        if (lpm_prevent_sleep) {
-            lpm_set(LPM_IDLE);
-        }
-        else {
-            lpm_set(LPM_IDLE);
-            /* lpm_set(LPM_SLEEP); */
-            /* lpm_set(LPM_POWERDOWN); */
-        }
+        lpm_set(LPM_SLEEP);
     }
 }
 
